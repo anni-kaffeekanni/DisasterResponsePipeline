7b43e53cc47193a3ac073795d6705ba3a51490d2
anni-kaffeekanni <64602127+anni-kaffeekanni@users.noreply.github.com>
2023-01-23 12:05:18 +0100

    Add files via upload

--- data/process_data.py
+++ data/process_data.py
@@ -60,17 +60,21 @@ def clean_data(df):
         categories[column] = categories[column].str.strip('-'+column)
         categories[column] = categories[column].astype(int)
 
-    print(categories.head())
-
     # 3. Filter out all rows in the categories df, that have no binary entry
     categories = pd.DataFrame(data=np.where(categories.values >=1,1,0), columns=categories.columns)
 
-    print(categories.head())
-
     df = pd.concat([df.drop(columns=['categories']),categories], axis=1)
-    
-    return df
 
+    # 4. handle NAN values
+    df = df.dropna(subset=categories.columns, how='all')
+    df = df.dropna(subset=['message'])
+
+    if df[categories.columns].isna().any().any():
+        print('dataset still contains NaN values')
+
+    #df.reset_index(inplace=True)
+
+    return df
 
 def save_data(df, database_filename):
     ''' This function saves the processed data into a database
@@ -82,7 +86,7 @@ def save_data(df, database_filename):
     
     engine = create_engine(f'sqlite:///{database_filename}.db')
     
-    df.to_sql('messages_dataset', engine, index=False)
+    df.to_sql('messages_dataset', engine, index=False, if_exists='replace')
               
     pass  
 
@@ -115,7 +119,7 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
         
